7963b6415f7d07a3aa84d0dadb06bf04f3512c14
rock1909 <roket1909@mail.com>
2023-10-08 13:40:10 -0300

    complete sign up page fuctionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': { //* Requesting everytimg there is ' /api ' go to ' http://localhost:3000 */
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+
   plugins: [react()],
 })
