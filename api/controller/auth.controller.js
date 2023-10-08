352de50582ba6116a5467292acfb3ff3493a650d
rock1909 <roket1909@mail.com>
2023-10-08 15:34:43 -0300

    complete sign api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -12,4 +15,23 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error);
     }    
+};
+
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body; // Get data from request.body to email and password
+    try { // Authenticate users by email and password
+      const validUser =  await User.findOne({ email }); // Check email in mongoose
+      if (!validUser) return next(errorHandler(404, 'User not found!'));
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+      const { password: pass, ...rest } = validUser._doc;
+      res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest); 
+    } catch (error) {
+        next(error);
+    }
 };
